5cd74f6271b639f64e4b9a18fa5d1f1541cdf660
Fabio Mora <andunker@hotmail.com>
2020-08-11 18:53:23 -0500

    init

---
+++ src/test/java/com/andunker/mercadolibre/MercadoLibreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andunker.mercadolibre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoLibreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
