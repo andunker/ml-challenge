6b67d6df4e24ddabcd4d289abb514d8e0c42aa74
Fabio Mora <andunker@hotmail.com>
2020-08-12 00:46:25 -0500

    Docker

---
+++ script.sql
@@ -0,0 +1,8 @@
+CREATE TABLE public.items
+(
+    id VARCHAR(30) NOT NULL,
+    value text NULL DEFAULT NULL,
+    created_at TIMESTAMP(6) NULL DEFAULT NULL,
+    modified_at TIMESTAMP(6) NULL DEFAULT NULL,
+    CONSTRAINT bills_pkey PRIMARY KEY (id)
+);
